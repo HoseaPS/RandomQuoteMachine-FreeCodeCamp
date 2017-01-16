7e96357c15deb25774e3f300663b035e54e6d328
Oséias Paulo da Silva <hoseaps@gmail.com>
2017-01-16 20:31:02 -0200

    #

---
+++ README.txt
@@ -0,0 +1,5 @@
+The third project made with bootstrap and jquery at Free Code Camp by Oséias Paulo da Silva
+
+It is a Random Quote Machine with a button to tweet out the current quote.
+
+Credits: www.freecodecamp.com
---
+++ js/ajax.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	
+
+	$.ajax({
+	    headers: {
+	      	"X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V", 
+	    	Accept: "application/json",
+	      	"Content-Type": "application/x-www-form-urlencoded"
+	    },    
+	    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',  
+	    success: function(data) {
+
+	    	var frase = JSON.parse(data);
+	    	var quote = frase.quote;
+	    	var author = frase.author
+	    	$("#total-quote").addClass('animated fadeInDown');
+	    	$("#quote").text("\""+quote+"\"");
+	    	$("#author").text("- "+author);
+	    	$("#twitter-button").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + quote + '" ' +"- "+ author));
+
+	    }
+	});
+
+	$("#quote-button").on("click", function(){
+
+		$("#total-quote").removeClass('animated fadeInDown');
+
+		$.ajax({
+		    headers: {
+		      	"X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V", 
+		    	Accept: "application/json",
+		      	"Content-Type": "application/x-www-form-urlencoded"
+		    },    
+		    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',  
+		    success: function(data) {
+
+		    	var frase = JSON.parse(data);
+		    	var quote = frase.quote;
+		    	var author = frase.author
+
+		    	$("#total-quote").addClass('animated fadeInDown');
+		    	$("#quote").text("\""+quote+"\"");
+		    	$("#author").text("- "+author);
+		    	$("#twitter-button").attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + quote + '" ' +"- " + author));
+		    }
+		});
+	});
+});
